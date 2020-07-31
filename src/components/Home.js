35b693cf1c909f3de138b743ad49b3f2f31bd7a4
Basil <Vaseokk@gmail.com>
2020-07-31 12:53:33 +0300

    solved the bugs with positionig and the toggle menu button

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,19 +9,18 @@ const Home = () => {
   return (
     <div className="home">
       <div className="screen">
-          <div className="introduction-section">Mereu între prieteni</div>
-          <div className="know-more">Află mai mult despre Berea Chișinău</div>
-          <Link to="/produse">
-            <button className="massive ui button">Află Mai Mult</button>
-          </Link>
-          <div className="know-more">Vino la Turul Berii</div>
-          <Link to='/turul-berii'>
-            <button className="massive ui button">Rezervă</button>
-          </Link>
-
-          <div>
-            <Footer className="home-footer" />
-          </div>
+        <div className="introduction-section">Mereu între prieteni</div>
+        <div className="know-more">Află mai mult despre Berea Chișinău</div>
+        <Link to="/produse">
+          <button className="massive ui button">Află Mai Mult</button>
+        </Link>
+        <div className="know-more">Vino la Turul Berii</div>
+        <Link to="/turul-berii">
+          <button className="massive ui button">Rezervă</button>
+        </Link>
+        <div>
+          <Footer className="home-footer" />
+        </div>
       </div>
       <img className="backbeer" src={background_img} alt="" />
     </div>
