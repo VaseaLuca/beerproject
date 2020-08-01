7e7f3275d20b357439981b38b53fe5517ba33ea2
Basil <Vaseokk@gmail.com>
2020-08-01 21:55:06 +0300

    added some dato on Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import BereaNoastra from './BereaNoastra';
-import Product from './Product';
+
+import './Contact.css';
+
 const Contact = () => {
   return (
     <div>
-      <BereaNoastra />
-      <h1>Contact</h1>
-      <h1>Contact</h1>
-      <h1>Contact</h1>
-      <h1>Contact</h1>
-      <Product />
+      <div className="background-block">
+        <div className="main-constructor">
+          <div className="contact-list">
+            <ul>
+              <li>
+                Linia Fierbinte <br />
+                <a href="055 8003 4555"> 055 8003 4555</a>
+              </li>
+              <li>
+                <i class="telegram icon"></i> Bere Ohișin
+              </li>
+              <li>Pentru Programare Vizite la Fabrică</li>
+              <li>Turul Berii Chișinău</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
