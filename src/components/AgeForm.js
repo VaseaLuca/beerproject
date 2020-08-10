e564a41d32401ee0de0573ad29e17863abcd9af6
Basil <Vaseokk@gmail.com>
2020-08-10 17:30:29 +0300

    updated the age gate

--- src/components/AgeForm.js
+++ src/components/AgeForm.js
@@ -11,32 +11,45 @@ class AgeForm extends React.Component {
 
   }
 
-dayHandler = (e) => {
-  this.setState({ day: e.target.value})
-}
 
-monthHandler = (e) => {
-  this.setState({ month: e.target.value })
-}
+render() {  
 
-yearHandler = (e) => {
-  this.setState({ year: e.target.value })
-}
-
-render() {
   return (
     <div className="age-form">
       <div className="age-block">
         <div className="greetings">Bine Ai Venit</div>
         <form action="" noValidate>
           <div className="select-age">
-            <input onChange={this.dayHandler} className="birth-check" type="number" placeholder='ZZ' maxLength='2' min='1' max='31' />
-            <input onChange={this.monthHandler} className="birth-check" type="number" placeholder='LL' maxLength='2'/>
-            <input onChange={this.yearHandler} className="birth-check" type="number" placeholder='AAAA' maxLength='4'/>
+
+                              {     /* Day */     }
+            {this.state.day > 0 && this.state.day < 32 ? 
+              <input onChange={(e) => this.setState({ day: e.target.value })} className="birth-check" type="number" placeholder='ZZ' maxLength='2' min='1' max='31' value={this.state.day} style={{backgroundColor: '#072e70'}} /> 
+              : 
+              <input onChange={(e) => this.setState({ day: e.target.value })} className="birth-check" type="number" placeholder='ZZ' maxLength='2' min='1' max='31' value={this.state.day} style={{backgroundColor: '#aa0024'}} />
+            }
+                                  {     /* Month */     }
+            { this.state.month > 0 && this.state.month < 13 ?
+               <input onChange={(e) => this.setState({ month: e.target.value })} className="birth-check" type="number" placeholder="LL" maxLength="2" min="1" max="12" value={this.state.month} style={{backgroundColor: '#072e70'}}/>
+               :
+               <input onChange={(e) => this.setState({ month: e.target.value })} className="birth-check" type="number" placeholder="LL" maxLength="2" min="1" max="12" value={this.state.month} style={{backgroundColor: '#aa0024'}}/>
+            }
+
+                                   {     /* Year */     }
+            { this.state.year > 1939 && this.state.month < new Date().getFullYear() ? 
+              <input onChange={(e) => this.setState({ year: e.target.value })} className="birth-check" type="number" placeholder="AAAA" maxLength="4" min="1940" max={new Date().getFullYear()} value={this.state.year} style={{backgroundColor: '#072e70'}} />
+               :
+             <input onChange={(e) => this.setState({ year: e.target.value })} className="birth-check" type="number" placeholder="AAAA" maxLength="4" min="1940" max={new Date().getFullYear()} value={this.state.year} style={{backgroundColor: '#aa0024'}} /> 
+            }
             <div className="warning-message">
               Introduceți ziua, luna și anul nașterii
             </div>
-            <div className='line'><hr/></div>
+            <div className="line">
+              <hr />
+            </div>
+            {this.state.day && this.state.month && this.state.year ?
+              <div><button className='enter'>Intră</button></div> :
+              null
+              }
           </div>
         </form>
       </div>
